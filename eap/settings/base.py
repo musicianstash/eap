109db8dc04cc9be87b11e1f3ae9979970828770b
sitegroove <grabnarrok@gmail.com>
2016-04-17 22:14:20 +0200

    added timezone into environment variable

--- eap/settings/base.py
+++ eap/settings/base.py
@@ -226,4 +226,4 @@ CELERY_RESULT_BACKEND = os.getenv('EAP_CELERY_RESULT_BACKEND', 'redis://localhos
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Africa/Nairobi'
+CELERY_TIMEZONE = os.getenv('EAP_CELERY_TIMEZONE', 'Africa/Nairobi')
