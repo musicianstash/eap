933b2e872c7a2a2e6b383362c29cc11327862a27
sitegroove <grabnarrok@gmail.com>
2016-05-02 17:38:18 +0200

    changed deprecated core.context_processors to template.context_processors

--- app/eap/settings/base.py
+++ app/eap/settings/base.py
@@ -105,12 +105,12 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.contrib.auth.context_processors.auth',
-                'django.core.context_processors.request',
-                'django.core.context_processors.debug',
-                'django.core.context_processors.i18n',
-                'django.core.context_processors.media',
-                'django.core.context_processors.static',
-                'django.core.context_processors.tz',
+                'django.template.context_processors.request',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
                 'eap.contrib.base.context_processors.item_categories',
                 'eap.contrib.base.context_processors.popular_categories',
