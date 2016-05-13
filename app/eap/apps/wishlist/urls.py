512c05d85172444a6ec8f9a42cda53bc26fe43d0
sitegroove <grabnarrok@gmail.com>
2016-05-13 12:51:39 +0200

    deleted wishlist

--- app/eap/apps/wishlist/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.ArticlesListView.as_view(), name='articles'),
-    url(r'^(?P<slug>[-\w\d]+).html', views.ArticleView.as_view(), name='article'),
-]
