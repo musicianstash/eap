24a91baa011a8584c2ca24cd83964bb0163eb4f9
sitegroove <grabnarrok@gmail.com>
2016-05-13 22:57:34 +0200

    init

--- app/eap/apps/sync/management/commands/import_test_crawler_items.py
+++ app/eap/apps/sync/management/commands/import_test_crawler_items.py
@@ -2,6 +2,7 @@
 import json
 
 from django.core.management.base import BaseCommand
+from ...item import save_crawler_item
 
 import requests
 
@@ -11,7 +12,13 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         url = 'https://www.dropbox.com/s/m0c3jwm35kmtajz/music_items.json?dl=1'
         json_text = requests.get(url)
-        json_items = json.loads(json_text)
+        json_items = json.loads(json_text.text)
 
         for json_item in json_items:
-            print(json_item)
+            if 'electric guitar' not in json_item['classifiers']:
+                continue
+
+            try:
+                save_crawler_item(json_item)
+            except:
+                print("Something wrong: " + json_item['name'])
